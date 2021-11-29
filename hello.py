eda8665239ef12137ccdd12517abd540ab05ce87
arsKalishoek <99069312@mydavinci.nl>
2021-11-29 09:41:52 +0100

    hello.py toegevoegd

---
+++ hello.py
@@ -0,0 +1,17 @@
+import tkinter
+window = tkinter.Tk()
+
+window.title("Hello")
+window.config(bg= "black")
+window.geometry("100x100")
+
+widget = tkinter.Label(
+    window,
+    text = "Hello\n tkinter!\n", 
+    bg = "red",
+    fg = "white",
+    font= ('Helvetica 12 bold')).place(relx=.25, rely=.2
+    
+    )
+        
+window.mainloop()
